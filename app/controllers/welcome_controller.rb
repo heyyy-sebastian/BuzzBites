608b6f6dbe012baf826ecc57a52296aaab1493bf
Reggie Kim <reggiekim@gmail.com>
2016-05-23 12:21:55 -0400

    fix figaro for js

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    mapsApi = ENV['googlemaps_api_key']
-    url = "https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:07631&key="+mapsApi
-    data = HTTParty.get(url)
-    myLongitude = data['results'][0]['geometry']['location']['lng'];
-    myLatitude = data['results'][0]['geometry']['location']['lat'];
+
   end
 
   def create
