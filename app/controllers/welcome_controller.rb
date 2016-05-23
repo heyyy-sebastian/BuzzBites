2b183cedf95d564a1bc2e42152ad2ec72496a7d8
Meke Brown <mekesiabrown@gmail.com>
2016-05-23 17:20:15 -0400

    Add/update code for the sign up for users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,34 @@
 class WelcomeController < ApplicationController
   def index
-
+    @user = User.new
   end
 
   def create
-    user = params['user']
-    if user['password'] == user['password_confirmation']
-      newUser = User.create(name: user['name'], email: user['email'], username: user['username'], password: user['password'])
+    @user = params['user']
+    if @user['password'] == @user['password_confirmation']
+      newUser = User.create(name: @user['name'], email: @user['email'], username: @user['username'], password: @user['password'], loginnum: 1)
+      # newUser = User.create(user_params)
       if newUser
-        BuzzbitesMailer.buzzbites_welcome(user['email']).deliver_now
+        BuzzbitesMailer.buzzbites_welcome(@user['email']).deliver_now
+      else
+        render "Incorrect information"
       end
     end
+    # byebug
     redirect_to(:back)
   end
+
+  def put
+    # Add one to each login a user has
+    username = params['username']
+    password = params['password']
+    loginAttempt = User.update(username: username,
+                                password: password,
+                                loginnum: loginnum)
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :username, :password)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   validates :email,
       presence: true,
-      uniqueness: { case_sensitive: false},
+      uniqueness: {case_sensitive: false},
+      uniqueness: true,
       format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
 
   validates :password,
       presence: true,
-      length: { minimum: 8 }
+      length: { minimum: 8 },
+      format: { with: /[A-Za-z0-9._]/ }
 end
