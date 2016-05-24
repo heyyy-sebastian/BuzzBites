34e730885feaaa23765c8b07b4e3d3689d398447
Reggie Kim <reggiekim@gmail.com>
2016-05-24 17:11:24 -0400

    add modal

--- app/assets/javascripts/userform.js
+++ app/assets/javascripts/userform.js
@@ -1,25 +1,15 @@
 "use strict";
 $(document).ready(function() {
+
   var signUpClick = $("li#signUpClick");
   var loginClick = $("li#loginClick");
-  var divUserSignUp = $("div#userSignUp");
-  var divUserLogin = $("div#userLogin");
-  var cancelButton = $(".cancel-button");
-
 
   signUpClick.click(function() {
-    divUserLogin.hide("fast");
-    divUserSignUp.show("fast");
+    $('#modal1').openModal();
   });
 
   loginClick.click(function() {
-    divUserSignUp.hide("fast");
-    divUserLogin.show("fast");
-  });
-
-  cancelButton.on("click", function() {
-    divUserSignUp.hide("fast");
-    divUserLogin.hide("fast");
+    $('#modal2').openModal();
   });
 
 });
