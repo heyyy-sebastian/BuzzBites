4547f5ec51bad75a94d59c5a35ac28127450e0dc
Reggie Kim <reggiekim@gmail.com>
2016-05-25 10:58:59 -0400

    add media query fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/' => 'welcome#index'
+
   root 'welcome#index'
   patch 'welcome/login', to: 'welcome#login'
   resources :welcome, only: [:index, :create, :login, :logout]
